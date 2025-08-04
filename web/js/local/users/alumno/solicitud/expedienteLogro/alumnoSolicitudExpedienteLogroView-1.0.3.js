b2e69d23f1b9d3a9161a5626ede7786fda62897c
unknown <ricardo.contreras@usach.cl>
2025-08-04 12:00:49 -0400

    Backup autom├ítico 04-08-2025 12:00:49,37

--- web/js/local/users/alumno/solicitud/expedienteLogro/alumnoSolicitudExpedienteLogroView-1.0.3.js
+++ web/js/local/users/alumno/solicitud/expedienteLogro/alumnoSolicitudExpedienteLogroView-1.0.3.js
@@ -94,14 +94,6 @@ function generaPagoArancel() {
 }
 
 function abrirPaso(numero) {
-    console.log("Llamada paso");
-    // Cierra todos los paneles primero (opcional, si usas data-bs-parent)
-    const paneles = document.querySelectorAll('.accordion-collapse');
-    paneles.forEach(p => {
-      const instancia = bootstrap.Collapse.getOrCreateInstance(p);
-      instancia.hide();
-    });
-
     // Abre el panel deseado
     const target = document.getElementById(`collapse${numero}`);
     const collapseInstance = bootstrap.Collapse.getOrCreateInstance(target);
